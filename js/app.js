28052a3d1f7c7572e736b1b19a664600be2e0c80
NormixPach <normpach30@gmail.com>
2017-12-05 13:20:20 -0600

    modificaciones

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
+function myFunction() {
+  var x = document.getElementById('form');
+  x.style.display = 'block';
 
+ var y = document.getElementsByClassName('lista');
+  y.style.display = 'none';
+}
